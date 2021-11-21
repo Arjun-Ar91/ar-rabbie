5343ab040e8fac2bdffccad1d460358c2d98dd48
Arjun-Ar91 <arjun1120@gmail.com>
2021-11-21 10:53:41 +0000

    3rd commit

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ class ActionHelper {
     static sendText(locator, inputText) {
         console.log('******* i am in SENDTEXT');
         console.log('******* i am in SENDTEXT');
+        console.log('******* i am in SENDTEXT');
         $(locator).addValue(inputText);
     }
 
